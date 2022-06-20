d252d4200ed1e30b3b5850d0b442b84a3aa51f8a
felix1805 <felix1805@gmail.com>
2022-06-20 13:12:26 -0400

    added files from previous directory for initial github upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel} from '@mui/material';
+import './App.css';
+import Todo from './Todo'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  console.log('hello', input)
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    console.log('alien');
+    setTodos([...todos, input]);
+    setInput('');
+  }
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <form>
+        <FormControl>
+          <InputLabel>✅ Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">Add Todo
+        </Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo}/>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBFPDRNxD3ErUugexBXsxqunGh_aZFt7zo",
+  authDomain: "todo-app-522ad.firebaseapp.com",
+  projectId: "todo-app-522ad",
+  storageBucket: "todo-app-522ad.appspot.com",
+  messagingSenderId: "384090076448",
+  appId: "1:384090076448:web:2811231920aff9a8f69345",
+  measurementId: "G-93CTRMC3QH"
+});
+
+const db = firebaseApp.firestore();
+
+export default db ;
