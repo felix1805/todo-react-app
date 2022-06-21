0cd19a7291794edd6c68da9791936e811f005df2
felix1805 <felix1805@gmail.com>
2022-06-21 10:31:43 -0400

    firebase functional on previous version 8.1.0

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBFPDRNxD3ErUugexBXsxqunGh_aZFt7zo",
   authDomain: "todo-app-522ad.firebaseapp.com",
   projectId: "todo-app-522ad",
@@ -13,10 +8,7 @@ const firebaseConfig = {
   messagingSenderId: "384090076448",
   appId: "1:384090076448:web:2811231920aff9a8f69345",
   measurementId: "G-93CTRMC3QH"
-};
+});
 
-// Initialize Firebase
-const db = initializeApp(firebaseConfig);
-const analytics = getAnalytics(db);
-
+const db = firebaseApp.firestore();
 export default db ;
